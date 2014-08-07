e5dc3bd677164f4f67c3572c4108179d8f372154
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-08-08 00:42:59 +0400

    Add angular devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Wwwonder::Application.routes.draw do
 
-  # devise_for :users
+  devise_for :users
 
   resource :welcome, only: [:show]
   root to: 'welcome#show'
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :users, only: [:index, :show, :update]
+      resources :users, only: [:index, :show, :create, :update]
     end
   end
 end
