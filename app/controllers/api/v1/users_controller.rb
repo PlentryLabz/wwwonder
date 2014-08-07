8f4f3740115844682a279d71d650e054360a8899
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-08-08 01:01:01 +0400

    fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,24 +10,4 @@ class Api::V1::UsersController < Api::V1::ApplicationController
     respond_with(@user, location: nil)
   end
 
-  def create
-    @user = User.new(params[:user])
-
-    if @user.save
-      respond_with(@user, location: nil)
-    else
-      render json: {errors: @user.errors.full_messages}, status: 422
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update_attributes(params[:user])
-      respond_with(@user, location: nil)
-    else
-      render json: {errors: @user.errors.full_messages}, status: 422
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Wwwonder::Application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create, :update]
+      resources :users, only: [:index, :show]
     end
   end
 end
