38a59f9fdac868218b0947417c9b048f6737b621
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-08-08 10:15:35 +0000

    Image uploader to S3 [done].

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJZ4IY26MP7IK3MYQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
-    :region                 => 'eu-east-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAJZ4IY26MP7IK3MYQ',
+    :aws_secret_access_key  => 'TXoer1ZAZrNs1U098P929yo3mpGMpUlJDIdN3Fr8',
+    :region                 => 'us-east-1',
   }
-  config.fog_directory  = 'wwwonder'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_directory  = 'wwwonder'
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
