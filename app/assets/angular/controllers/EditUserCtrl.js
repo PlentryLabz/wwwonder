a5121684197e66b46bfe1ce2bcf50a205a67dc71
danillewin <danillewin@gmail.com>
2014-08-23 18:30:25 +0400

    *

--- app/assets/angular/controllers/EditUserCtrl.js
+++ app/assets/angular/controllers/EditUserCtrl.js
@@ -1,9 +1,8 @@
 App.controller("EditUserCtrl", function ($scope, Users) {
   $scope.model = {}
 
-    var user = Users.get({id:$routeParams.id}, function () {
-      
-    });
+    var user = Users.get({id:$routeParams.id});
+    
     user.$promise.then(function (data) {
       $scope.model.lastName = data.last_name;
       $scope.model.img = data.pic;
--- app/assets/angular/controllers/UserCtrl.js
+++ app/assets/angular/controllers/UserCtrl.js
@@ -1,18 +1,14 @@
-(function() {
+App.controller("UserCtrl", function ($scope, $routeParams, Users) {
 
-  App.controller("UserCtrl", function ($scope, $routeParams, Users) {
+  $scope.model = {}
 
-    $scope.model = {}
-
-    var user = Users.get({id:$routeParams.id}, function () {
-      
-    });
-    user.$promise.then(function (data) {
-      $scope.model.lastName = data.last_name;
-      $scope.model.img = data.pic;
-      $scope.model.email = data.email;
-    })
-
+  var user = Users.get({id:$routeParams.id}, function () {
+    
+  });
+  user.$promise.then(function (data) {
+    $scope.model.lastName = data.last_name;
+    $scope.model.img = data.pic;
+    $scope.model.email = data.email;
   })
 
-})()
+})
