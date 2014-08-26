91754a1cad2322d379707df20f117e34d6f9e130
danillewin <danillewin@gmail.com>
2014-08-27 01:18:34 +0400

    New model for user edit.

--- app/assets/angular/models/UsersModel.js
+++ app/assets/angular/models/UsersModel.js
@@ -3,12 +3,12 @@
 App.service('Users', function ($resource) {
     var users = $resource('http://localhost:3000/api/v1/users/:id', 
     {
-
+      
     }, 
     {
         update: {
             method: "PUT",
-            url: "http://localhost:3000/users"
+            url: "http://localhost:3000/api/v1/users"
         },
         current: {
             method: "GET",
