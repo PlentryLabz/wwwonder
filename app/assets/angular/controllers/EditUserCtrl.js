9cdae9cbefc945dff141f97788cd59f5ad22088d
danillewin <danillewin@gmail.com>
2014-08-26 23:19:25 +0400

    *

--- app/assets/angular/controllers/EditUserCtrl.js
+++ app/assets/angular/controllers/EditUserCtrl.js
@@ -1,10 +1,11 @@
 App.controller("EditUserCtrl", function ($scope, Users) {
     $scope.model = new Users();
+    $scope.model.user = {};
 
     var currentUser = Users.current();
 
     currentUser.$promise.then(function (data) {
-        $scope.model.user.last_name = data.last_name;
+        $scope.model.user.lasе_name = data.last_name;
         $scope.model.user.first_name = data.first_name;
         $scope.model.user.instagram_link = data.instagram_link;
         $scope.model.user.vk_link = data.vk_link;
